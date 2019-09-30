10b3352d3194d17c898e4bd0ef7536397b9ab0e4
ReaziS <kost9n98@yandex.ru>
2019-09-30 16:09:52 +0300

    feat: complete all test wihout '|' symbol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ // const check = (str, bracketsConfig) => {
+  let expected = [];
+  let ret = '';
+  let br;
+  let brackets = {};
+  bracketsConfig.map(_ => {
+    brackets[_[0]] = _[1];
+  });
+  br = str.split('');
+  br.map(item => {
+    if (brackets[item]) {
+      expected.push(brackets[item]);
+    } else {
+      if (item === expected[expected.length-1]) {
+        expected.pop();
+      }
+    }
+  });
+  return expected.length == 0 ? true : false; 
 }
+//console.log(check('|()|', [['(', ')'], ['|', '|']]));
